7616d8d82407effd4689c1d4be02b6e1f3fadad7
crd <806163009@qq.com>
2023-04-12 20:34:19 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author: 陈瑞东
+ * @create: 2023-04-12 20:31
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
